ca5159c527c13cedba299f2a088eb918964e2690
David Todd <daveytodd@gmail.com>
2026-09-14 07:41:02 +1000

    Initial Commit

---
+++ Shot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shot
+//
+//  Created by David Todd on 14/9/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shot/ShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShotApp.swift
+//  Shot
+//
+//  Created by David Todd on 14/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
